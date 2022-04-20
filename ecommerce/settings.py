84e2f3d70801ddd19fef292e37eb2290d1e597f9
Mohamed Abdesselam <mm.abdesselam@gmail.com>
2022-04-20 18:49:55 +0200

    creation de la branche prod

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f9*kw*^8*y1k68cpk_3r5s8np4av$q$6=@cvl!1=2sb$wl-96u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://mamco.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', ' mamco.herokuapp.com/']
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
